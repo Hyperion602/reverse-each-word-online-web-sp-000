46fe1f46385cd419a5837afa8e9283617e0d8d32
James Harris <mharris9608@yahoo.com>
2020-12-28 18:12:36 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
 def reverse_each_word(array)
-  reverse = [array.split(" ")]
   reversed = []
-  reverse.each do |word|
+  array.split(" ").each do |word|
     reversed << word.reverse
   end
   reversed
